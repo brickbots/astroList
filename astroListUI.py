c651e934fa3517e7126693a0108a4e5471c2c0c0
brickbots <rich@brickbots.com>
2018-05-20 14:02:59 +1000

    Firming up UI params for 1024x600

--- astroListUI.py
+++ astroListUI.py
@@ -2,8 +2,8 @@ from Tkinter import *
 
 BUTTON_BG='#330000'
 BUTTON_FG='#990000'
-BUTTON_FONT=('Arial Unicode MS', -18, 'bold')
-BUTTON_WIDTH=11
+BUTTON_FONT=('Arial Unicode MS', -16, 'bold')
+BUTTON_WIDTH=10
 BUTTON_HEIGHT=2
 BUTTON_BORDER=2
 BUTTON_BORDER_COLOR='#000000'
@@ -14,7 +14,7 @@ DATA_BG='#000000'
 class AstroList(object):
     def __init__(self, parent):
         self._parent = parent
-        self._mainLayout = Frame(self._parent, height=560, width=510, background="#000000")
+        self._mainLayout = Frame(self._parent, height=600, width=510, background="#000000")
         self._mainLayout.grid_propagate(0)
         self._mainLayout.grid()
 
@@ -40,27 +40,27 @@ class AstroList(object):
         self.button4.configure(text="CLOSE", font=BUTTON_FONT, background=BUTTON_BG, foreground=BUTTON_FG,
                                height=BUTTON_HEIGHT, width=BUTTON_WIDTH, highlightthickness=BUTTON_BORDER,
                                highlightbackground=BUTTON_BORDER_COLOR, highlightcolor=BUTTON_BORDER_COLOR)
-        self.button4.grid(column=3, row=0, columnspan=2)
+        self.button4.grid(column=3, row=0, columnspan=2, sticky=N+S+E+W)
 
         self.button5 = Label(self._mainLayout)
         self.button5.configure(text="", font=BUTTON_FONT, background=BUTTON_BG, foreground=BUTTON_FG,
                                height=BUTTON_HEIGHT * 4, width=6, highlightthickness=BUTTON_BORDER,
                                highlightbackground=BUTTON_BORDER_COLOR, highlightcolor=BUTTON_BORDER_COLOR)
-        self.button5.grid(column=4, row=3, rowspan=2, sticky=N+S+E+W)
+        self.button5.grid(column=4, row=1, rowspan=2, sticky=N+S+E+W)
 
         self.button6 = Label(self._mainLayout)
         self.button6.configure(text="E\nN\nT\nE\nR", font=BUTTON_FONT, background=BUTTON_BG, foreground=BUTTON_FG,
                                height=BUTTON_HEIGHT * 4, width=6, highlightthickness=BUTTON_BORDER,
                                highlightbackground=BUTTON_BORDER_COLOR, highlightcolor=BUTTON_BORDER_COLOR)
-        self.button6.grid(column=4, row=5, rowspan=2, sticky=N+S+E+W)
+        self.button6.grid(column=4, row=3, rowspan=2, sticky=N+S+E+W)
 
         self.cornerSpace=Label(self._mainLayout)
         self.cornerSpace.configure(text="", font=BUTTON_FONT, background=DATA_BG, foreground=DATA_FG,
-                               height=BUTTON_HEIGHT, width=6, highlightthickness=BUTTON_BORDER,
-                               highlightbackground=BUTTON_BORDER_COLOR, highlightcolor=BUTTON_BORDER_COLOR)
-        self.cornerSpace.grid(column=5, row=1)
+                                   height=BUTTON_HEIGHT, width=6, highlightthickness=BUTTON_BORDER,
+                                   highlightbackground=BUTTON_BORDER_COLOR, highlightcolor=BUTTON_BORDER_COLOR)
+        self.cornerSpace.grid(column=5, row=5)
 
-        self._dataFrame = Frame(self._mainLayout, height=500, width=440)
+        self._dataFrame = Frame(self._mainLayout, height=554, width=440)
         self._dataFrame.configure(background='#111111')
         self._dataFrame.grid_propagate(0)
         self._dataFrame.grid(column=0, row=1, columnspan=4, rowspan=5)
